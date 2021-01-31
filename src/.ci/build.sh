0cd34cec240f75291268cdeb28abb329aef356f7
Cameron Tranthim-Fryer <devops.at.home@gmail.com>
2021-01-31 20:58:13 +0800

    update build

--- src/.ci/build.sh
+++ src/.ci/build.sh
@@ -10,7 +10,7 @@ function put_param () {
     --name "/codebuild/state/docker-monorepo/prev-git-sha" \
     --type "String" \
     --value "$GIT_SHA"\
-    --overwritez
+    --overwrite
   exit 0
 }
 
@@ -30,6 +30,30 @@ do
 
   echo ECR_REPOSITORY=$ECR_REPOSITORY
   echo VERSION=$VERSION
+
+  echo Building the amd64 Docker image on `date`
+  docker buildx build \
+    --platform linux/amd64 \
+    --output "type=image,push=true"        
+    --tag $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:latest-amd64
+    --tag $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:$VERSION-amd64 \
+  
+  echo Building the arm64 Docker image on `date`
+  docker buildx build \
+    --platform linux/arm64 \
+    --output "type=image,push=true"        
+    --tag $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:latest-arm64
+    --tag $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:$VERSION-arm64 \
+
+  echo Building the Docker manifest on `date` 
+  docker manifest create $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:latest-arm64 $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:latest-amd64    
+  docker manifest annotate --arch arm64 $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:latest-arm64
+  docker manifest annotate --arch amd64 $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:latest-amd64
+  docker manifest create $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:$VERSION-arm64 $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:$VERSION-amd64    
+  docker manifest annotate --arch arm64 $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:$VERSION-arm64
+  docker manifest annotate --arch amd64 $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY:$VERSION-amd64
+  docker manifest push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY
+  docker manifest inspect $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPOSITORY
 done;
 
 put_param
